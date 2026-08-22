bd573c131802c944a18765dafa70215efe7555bd
joaovpzdev <joaovpz.dev@gmail.com>
2026-08-22 12:12:21 -0300

    feat: config routers initial pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import HomePage from './pages/home';
+import LoginPage from './pages/login';
+import NotFoundPage from './pages/not-found';
 import SignupPage from './pages/signup';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SignupPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
