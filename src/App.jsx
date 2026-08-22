edbce016989666345df8d5d5ecb95e23413a0a02
joaovpzdev <joaovpz.dev@gmail.com>
2026-08-22 10:57:45 -0300

    feat: update colors dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
-  return <h1 className="text-red-500"> Hello world </h1>;
+  return <Button className="bg-primary-blue"> Hello world </Button>
 };
 
 export default App;
