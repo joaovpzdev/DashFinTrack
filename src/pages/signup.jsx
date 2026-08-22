a257dd241bf8a78e0c17b316a3ee8de833096eb8
joaovpzdev <joaovpz.dev@gmail.com>
2026-08-22 12:29:39 -0300

    feat: add password inputs with icons

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,4 +1,6 @@
-import { Link } from 'react-router';
+import { EyeIcon, EyeOffIcon } from 'lucide-react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import { Button } from '../components/ui/button';
 import {
@@ -12,6 +14,7 @@ import {
 import { Input } from '../components/ui/input';
 
 const SignupPage = () => {
+  const [passwordIsVisible, setPasswordIsVisible] = useState(false);
   return (
     <div className="flex flex-col h-screen w-screen items-center justify-center gap-3">
       <Card className="w-[500px]">
@@ -23,7 +26,16 @@ const SignupPage = () => {
           <Input placeholder="Nome" />
           <Input placeholder="Sobrenome" />
           <Input placeholder="Email" />
-          <Input type="password" placeholder="Senha" />
+          <div className="relative">
+            <Input type={passwordIsVisible ? 'text' : 'password'} placeholder="Senha" />
+            <Button
+              variant="ghost"
+              className="absolute bottom-0 right-0 top-0 my-auto mr-1 h-8 w-8 text-muted-foreground"
+              onClick={() => setPasswordIsVisible((prev)=> !prev)}
+            >
+              {passwordIsVisible ? <EyeIcon/> : <EyeOffIcon/>}
+            </Button>
+          </div>
         </CardContent>
         <CardFooter>
           <Button className="w-full"> Criar Conta </Button>
