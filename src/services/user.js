5b0684d688df93d97f47a4b162c7e5e0230cf25f
joaovpzdev <joaovpz.dev@gmail.com>
2026-08-25 10:35:15 -0300

    docs: add JS docs to user service

--- src/services/user.js
+++ src/services/user.js
@@ -1,6 +1,19 @@
 import { protectedApi, publicApi } from '../lib/axios';
 
 export const UserService = {
+  /**
+   * Cria um novo usuário no sistema.
+   * @param {object} input
+   * @param {string} input.firstName - Primeiro nome do usuário.
+   * @param {string} input.lastName - Sobrenome do usuário.
+   * @param {string} input.email - E-mail do usuário.
+   * @param {string} input.password - Senha do usuário.
+   * @param {string} input.passwordConfirmation - Confirmação da senha do usuário.
+   * @param {boolean} input.terms - Aceitação dos termos de uso.
+   * @returns {object} - Retorna um objeto com os dados do usuário criado e seus tokens.
+   * @return {string} response.tokens - Retorna os tokens de acesso e refresh do usuário.
+   */
+
   signup: async (input) => {
     const response = await publicApi.post('/users', {
       firstName: input.firstName,
@@ -15,7 +28,7 @@ export const UserService = {
       email: response.data.email,
       firstName: response.data.first_name,
       lastName: response.data.last_name,
-        tokens: response.data.tokens,
+      tokens: response.data.tokens,
     };
   },
   login: async (input) => {
@@ -24,13 +37,17 @@ export const UserService = {
       password: input.password,
     });
     return {
-        id: response.data.id,
-        email: response.data.email,
-        firstName: response.data.first_name,
-        lastName: response.data.last_name,
-        tokens: response.data.tokens,
+      id: response.data.id,
+      email: response.data.email,
+      firstName: response.data.first_name,
+      lastName: response.data.last_name,
+      tokens: response.data.tokens,
     };
   },
+  /**
+   * Retorna os dados do usuário autenticado.
+   * @returns {object} - Retorna um usuario autenticado com seus dados e tokens.
+   */
   me: async () => {
     const response = await protectedApi.get('/users/me');
     return {
