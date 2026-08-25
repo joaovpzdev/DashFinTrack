b02f3f62854f180a4de6d64be6f69d8c0a7508b7
joaovpzdev <joaovpz.dev@gmail.com>
2026-08-25 09:26:11 -0300

    feat: ensure access token is refresh using refresh token tkns on error 401

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { LOCAL_STORAGE_ACCESS_TOKEN_KEY } from '@/constants/local-storage';
+import { LOCAL_STORAGE_ACCESS_TOKEN_KEY, LOCAL_STORAGE_REFRESH_TOKEN_KEY } from '@/constants/local-storage';
 
 export const protectedApi = axios.create({
   baseURL: 'https://fullstackclub-finance-dashboard-api.onrender.com',
@@ -15,4 +15,35 @@ protectedApi.interceptors.request.use((request) => {
   if (!accessToken) return request;
   request.headers.Authorization = `Bearer ${accessToken}`;
   return request;
+});
+
+protectedApi.interceptors.response.use((response) => response, async (error) => {
+  const request = error.config;
+  //verificar se tenho refresh toje no local storage.
+  const refreshToken = localStorage.getItem(LOCAL_STORAGE_REFRESH_TOKEN_KEY);
+  if (!refreshToken) {
+    return Promise.reject(error);
+  }
+  //verificar se o erro é 401 (toke inválido ou inexistenete)
+  if (error.response.status === 401 && !request.retry && !request.url.includes('/users/refresh-token')) {
+    request.retry = true;
+    //se tiver o refresh token, fazer a atualoização do access token e refazer a requisição original.
+    try {
+      const response = await protectedApi.post('/users/refresh-token', {
+        refreshToken,
+      });
+      const newAccessToken = response.data.accessToken;
+      const newRefreshToken = response.data.refreshToken;
+      localStorage.setItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY, newAccessToken);
+      localStorage.setItem(LOCAL_STORAGE_REFRESH_TOKEN_KEY, newRefreshToken);
+      request.headers.Authorization = `Bearer ${newAccessToken}`;
+      return protectedApi(request);
+    } catch (refreshError) {
+      localStorage.removeItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY);
+      localStorage.removeItem(LOCAL_STORAGE_REFRESH_TOKEN_KEY);
+      console.error(refreshError);
+      return Promise.reject(refreshError);
+    }
+  }
+  return Promise.reject(error);
 });
